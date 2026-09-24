9b547ed45d3666fc24c4a28123272368734c8762
Eya Gobji <eya.gobji@esprit.tn>
2026-09-24 11:35:19 +0100

    Ajout des entités Vehicule, StatutVehicule, CategorieVehicule et config application.properties

---
+++ autoloc-api/src/main/java/tn/esprit/autoloc/domain/CategorieVehicule.java
@@ -0,0 +1,10 @@
+package tn.esprit.autoloc.domain;
+
+public enum CategorieVehicule {
+    CITADINE,
+    BERLINE,
+    SUV,
+    UTILITAIRE,
+    LUXE,
+    CABRIOLET
+}
---
+++ autoloc-api/src/main/java/tn/esprit/autoloc/domain/StatutVehicule.java
@@ -0,0 +1,9 @@
+package tn.esprit.autoloc.domain;
+
+public enum StatutVehicule {
+    DISPONIBLE,
+    LOUE,
+    EN_MAINTENANCE,
+    HORS_SERVICE,
+    RESERVE
+}
